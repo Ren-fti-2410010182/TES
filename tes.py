5f63e862891b194b970ced82c8e2e6635eb705e5
Azkaa Mosuta <azkaa.ms@gmail.com>
2026-03-24 11:47:25 +0800

    aku menambahkan beberapa baris kode untuk menyapa

--- tes.py
+++ tes.py
@@ -6,3 +6,11 @@ if angka % 2 == 0:
     print(f"{angka} adalah bilangan GENAP")
 else:
     print(f"{angka} adalah bilangan GANJIL")
+
+# hai reno kenalan yukk
+# hai reno kenalan yukk
+# hai reno kenalan yukk
+# hai reno kenalan yukk
+# hai reno kenalan yukk
+# hai reno kenalan yukk
+# hai reno kenalan yukk
