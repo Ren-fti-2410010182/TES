500318e5b905a13db269b55743260e4fb9f6f3a2
Azkaa Mosuta <azkaa.ms@gmail.com>
2026-03-24 11:01:13 +0800

    saya menambahkan program ganjil

--- tes.py
+++ tes.py
@@ -1,1 +1,8 @@
-    
+    # Program Ganjil Genap
+
+angka = int(input("Masukkan angka: "))
+
+if angka % 2 == 0:
+    print(f"{angka} adalah bilangan GENAP")
+else:
+    print(f"{angka} adalah bilangan GANJIL")
